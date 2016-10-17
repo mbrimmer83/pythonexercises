2219c9a4cf273a86887bd0f55fa23609a049de25
Matthew <mbrimmer1@gmail.com>
2016-10-16 22:35:49 -0400

    More try and except

--- try.py
+++ try.py
@@ -2,8 +2,13 @@ print "How old are you?"
 user_input = raw_input()
 
 try:
-    print user_input * user_input
-    print user_input
-    print user_input + user_input
-except:
-    print "Cant perform that operation"
+    inp = int(user_input)
+    print inp * inp
+except ValueError:
+    print "Please enter an integer"
+    user_input = raw_input()
+try:
+    inp = int(user_input)
+    print inp * inp
+except ValueError:
+    print "You are an idiot!"
